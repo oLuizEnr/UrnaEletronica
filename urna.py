9d5966cf0c5fe40e82ba79c9f42999ec139bcd96
Luiz Enrique <luizenrique80025030@gmail.com>
2025-06-01 23:08:14 -0300

    Estilização da janela inicial + janela cadastro

--- urna.py
+++ urna.py
@@ -12,6 +12,7 @@ janela.title("Urna eletrônica") # Titulo da window aberta pelo Sistema Operacio
 
 janela_largura = 800 # largura da window que se abre na tela
 janela_altura = 600 # altura da window que se abre na tela
+janela.resizable(False, False) # Impede alteração manual da largura e altura da janela
 
 # Criação das váriaveis para centralizar a janela
 largura_tela = janela.winfo_screenwidth() # largura da tela do usuário
@@ -26,8 +27,8 @@ saida_pdf.add_page() # Adciona uma página ao PDF
 
 # Váriaveis para funcionalidade do código
 with open("candidatos.json", "r", encoding="utf-8") as arquivo:
-    candidatos = json.load(arquivo)
-votacao_ativa = False
+    candidatos = json.load(arquivo) # Abre o json e insere os dados dele em candidatos
+votacao_ativa = False # Impede o usuário de ver os resultados antes de iniciar a votação
 
 # Definição das fontes
 fonte_titulo = font.Font(family="Helvetica", size=40, weight="bold")
@@ -37,35 +38,42 @@ fonte_pequena = font.Font(family="Helvetica", size=13)
 # Mostra a tela inicial
 def mostra_menu():
     # Definições básicas da janela atual
-    janela.configure(bg="#f0f0f0", padx=20, pady=60) # Adiciona fundo cinza claro e margem grande
-    janela.geometry(f"{janela_largura}x{janela_altura}+{posx}+{posy}") # Define a proporção e posição (centralizada) da janela 
+    janela.configure(bg="#f0f0f0", padx=20, pady=80) # Adiciona fundo cinza claro e margem grande
+    janela.geometry(f"{janela_largura}x{janela_altura}+{posx}+{posy}") # Define a proporção e posição (centralizada) da janela
 
-    label_menu = tk.Label(janela, text="Escolha uma opção:", bg="#f0f0f0", font=fonte_titulo) # "Caixa" p/ exibir texto ou imagem
+    label_menu = tk.Label(janela, text="Escolha uma opção:", width=18, height=2,
+                          font=fonte_titulo) # "Caixa" p/ exibir texto ou imagem
     label_menu.pack(pady=10) # Espaçamento entre o rótulo e os botões
 
-    botao1 = tk.Button(janela, text="Cadastro de Candidato", command=cadastra_candidato) # Botão que chama função determinada ao clicar
-    botao1.pack(pady=50) # Espaçamento vertical até o próximo widget na janela
+    frame_botoes = tk.Frame(janela, bg="#f0f0f0") # Container para alocar os botões em linha
+    frame_botoes.pack(expand=True, pady=20) # Espaçamento vertical até o próximo widget na janela
 
-    botao2 = tk.Button(janela, text="Iniciar Votação", command=iniciar_votacao) # Linha 45
-    botao2.pack(pady=5) # Linha 46
+    botao1 = tk.Button(frame_botoes, text="Cadastrar Candidatos", width=23, height=5, bg="#3d9dd4", fg="#ffffff",
+                        font=fonte_pequena, command=cadastra_candidato) # Botão que chama função determinada ao clicar
+    botao1.pack(side="left", padx=10, expand=True) # Botão alinhado a esquerda com um espaçamento horizontal
 
-    botao3 = tk.Button(janela, text="Encerrar Votação", command=encerrar_votacao) # Linha 45
-    botao3.pack(pady=5) # Linha 46
+    botao2 = tk.Button(frame_botoes, text="Liberar Votação", width=23, height=5, bg="#45a755", fg="#ffffff",
+                        font=fonte_pequena, command=liberar_votacao) # Linha 45
+    botao2.pack(side="left", padx=10, expand=True) # Linha 46
+
+    botao3 = tk.Button(frame_botoes, text="Ver Resultados", width=23, height=5, bg="#d49c14", fg="#ffffff",
+                        font=fonte_pequena, command=ver_resultados) # Linha 45
+    botao3.pack(side="left", padx=10, expand=True) # Linha 46
 
     janela.mainloop() # Mantém a janela rodando (sem isso ela abriria e fecharia logo em seguida, sem se quer ser visivel)
 
 # Exibe uma imagem em uma das janelas
 def exibir_imagens(tela, imagem_tk):
-    label_imagem = tk.Label(tela, image=imagem_tk) # Linha 42
+    label_imagem = tk.Label(tela) # Linha 42
+    label_imagem.config(image=imagem_tk)
     label_imagem.image = imagem_tk # Garante que a imagem não se perde no parâmetro do Label
-
-    label_imagem.pack(pady=5) # Linha 46
+    label_imagem.pack(fill="both", expand=True)
 
 # Carrega/Renderiza as imagens a partir do caminho delas
 def carregar_imagem(caminho):
     imagem = Image.open(caminho) # Recebe um caminho de arquivo e o renderiza como iamgem
 
-    imagem_redimensionada = imagem.resize((200, 200)) # Redimensiona, para tornar padrão
+    imagem_redimensionada = imagem.resize((200, 150)) # Redimensiona, para tornar padrão
     
     return ImageTk.PhotoImage(imagem_redimensionada) # Retorna a imagem totalmente tratada
 
@@ -76,22 +84,45 @@ def cadastra_candidato():
     janela_cadastro.protocol("WM_DELETE_WINDOW", janela.destroy) # Caso o usuário feche a janela filha, o programa para
 
     # Definições básicas da janela atual
-    janela_cadastro.title("Cadastro de Candidato") # Linha 9
-    janela_cadastro.configure(bg='#f0f0f0') # Linha 39
+    janela_cadastro.title("Urna eletrônica") # Linha 9
+    janela_cadastro.configure(bg='#f0f0f0', padx=30, pady=30) # Linha 39
     janela_cadastro.geometry(f"{janela_largura}x{janela_altura}+{posx}+{posy}") # linha 40
+    janela_cadastro.resizable(False, False) # Linha 15
 
     # Conteúdo da janela atual
-    tk.Label(janela_cadastro, text="Número do Candidato:").pack(pady=5) # Linha 42
-    entrada_numero = tk.Entry(janela_cadastro) # Input que recebe "Número do Candidato"
-    entrada_numero.pack(pady=5) # Linha 46
+    def voltar():
+        if entrada_nome.get() or entrada_numero.get() or entrada_partido.get():
+            confirmar = messagebox.askyesno("Confirmação", "Existem dados não salvos, voltar mesmo assim?")
+            if not confirmar:
+                return
+        janela_cadastro.destroy()
+        janela.deiconify() # Pop-up de verificação para voltar caso haja input preenchido
 
-    tk.Label(janela_cadastro, text="Nome do Candidato:").pack(pady=5) # Linha 42
-    entrada_nome = tk.Entry(janela_cadastro) # Input que recebe "Nome do Candidato"
-    entrada_nome.pack(pady=5) # Linha 46
+    botao_voltar = tk.Button(janela_cadastro, text="Voltar", bg="#e11c1c", fg="#ffffff", font=fonte_pequena,
+                                command=voltar) # Linha 45; Destrói janela atual e desesconde a principal
+    botao_voltar.pack(anchor="e") # Linha 46
 
-    tk.Label(janela_cadastro, text="Partido do Candidato:").pack(pady=5) # Linha 42
-    entrada_partido = tk.Entry(janela_cadastro) # Input que recebe "Partido do Candidato"
-    entrada_partido.pack(pady=5) # Linha 46
+    label_menu = tk.Label(janela_cadastro, text="Insira as informações:", width=18, height=2,
+                            font=fonte_titulo) # "Caixa" p/ exibir texto ou imagem
+    label_menu.pack(pady=8) # Espaçamento entre o rótulo e os botões
+
+    frame_geral = tk.Frame(janela_cadastro)
+    frame_geral.pack(fill="x")
+
+    frame_inputs = tk.Frame(frame_geral)
+    frame_inputs.pack(expand=True, side="left", anchor="sw")
+
+    tk.Label(frame_inputs, text="Número do Candidato:", font=fonte_pequena).pack(pady=6, anchor="w") # Linha 42
+    entrada_numero = tk.Entry(frame_inputs, width=50) # Input que recebe "Número do Candidato"
+    entrada_numero.pack(pady=6) # Linha 46
+
+    tk.Label(frame_inputs, text="Nome do Candidato:", font=fonte_pequena).pack(pady=6, anchor="w") # Linha 42
+    entrada_nome = tk.Entry(frame_inputs, width=50) # Input que recebe "Nome do Candidato"
+    entrada_nome.pack(pady=6) # Linha 46
+
+    tk.Label(frame_inputs, text="Partido do Candidato:", font=fonte_pequena).pack(pady=6, anchor="w") # Linha 42
+    entrada_partido = tk.Entry(frame_inputs, width=50) # Input que recebe "Partido do Candidato"
+    entrada_partido.pack(pady=6) # Linha 46
     
     # Dicionário para guardar as informações ultima imagem escolhida
     candidato_imgs = {
@@ -111,13 +142,19 @@ def cadastra_candidato():
             candidato_imgs["imagem_path"] = caminho # Guarda o caminho do arquivo da imagem atual
             candidato_imgs["imagem_tk"] = imagem_tk # Guarda a imagem renderizada atual
 
-            exibir_imagens(janela_cadastro, imagem_tk) # Mostra a ultima imagem escolhida pelo usuário
+            exibir_imagens(container_imagem, imagem_tk) # Mostra a ultima imagem escolhida pelo usuário
 
     # Conteúdo da janela - Continuação
-    tk.Button(janela_cadastro, text="Escolher imagem:", command=escolher_imagem).pack(pady=5) # Linha 45; Linha 46
+    frame_imagem = tk.Frame(frame_geral)
+    frame_imagem.pack(expand=True, side="right", anchor="s")
 
-    tk.Button(janela_cadastro, text="Voltar",
-        command=lambda: (janela_cadastro.destroy(), janela.deiconify())).pack(pady=5) # Linha 45; Destrói janela atual e desesconde janela principal; Linha 46
+    botao_imagem = tk.Button(frame_imagem, text="Escolher imagem:", bg="#d49c14", fg="#ffffff",
+              font=fonte_pequena, command=escolher_imagem)
+    botao_imagem.pack(pady=5) # Linha 45; Linha 46
+
+    container_imagem = tk.Frame(frame_imagem, width=200, height=150)
+    container_imagem.pack(pady=5)
+    container_imagem.pack_propagate(False)
 
     # Salva as informações atuais na lista candidato como um candidato novo
     def salvar_candidato():
@@ -161,17 +198,21 @@ def cadastra_candidato():
         with open("candidatos.json", "w", encoding="utf-8", ) as arquivo:
             json.dump(candidatos, arquivo, indent=4, ensure_ascii=False)
 
-    tk.Button(janela_cadastro, text="Salvar", command=salvar_candidato).pack(pady=5) # Linha 45; Linha 46
+    botao_salvar = tk.Button(janela_cadastro, text="Salvar", width=16, height=3, bg="#45a755", fg="#ffffff",
+              font=fonte_pequena, command=salvar_candidato)
+    botao_salvar.pack(side="bottom", anchor="s") # Linha 45; Linha 46
 
 # Inicia a votação e a formatação do PDF
-def iniciar_votacao():
+def liberar_votacao():
     # Váriavel que permite a execução de registrar voto e impede encerrar votação sem iniciar antes
     global votacao_ativa
     votacao_ativa = True
 
     # Informações de tempo
     agora = datetime.now() # Tempo atual do mundo, mês, dia, ano, hora, minuto, segundo, milisegundos
+    global data_votacao
     data_votacao = agora.date() # Apenas a data do tempo atual do mundo
+    global inicio_votacao
     inicio_votacao = agora.strftime("%H:%M:%S") # Apenas as horas, minutos e segundos do tempo atual do mundo
 
     # Formatações do PDF
@@ -193,20 +234,36 @@ def registrar_voto():
         janela_votacao.protocol("WM_DELETE_WINDOW", janela.destroy) # Linha 67
 
         # Definições básicas da janela atual
-        janela_votacao.title("Votação") # Linha 9
-        janela_votacao.configure(bg="#f0f0f0") # Linha 39
+        janela_votacao.title("Urna eletrônica") # Linha 9
+        janela_votacao.configure(bg='#f0f0f0', padx=0, pady=30) # Linha 39
         janela_votacao.geometry(f"{janela_largura}x{janela_altura}+{posx}+{posy}") # linha 40
+        janela_votacao.resizable(False, False) # Linha 15
 
         # Conteúdo da janela atual
+        def voltar():
+            if entrada_matricula.get() or entrada_voto.get():
+                confirmar = messagebox.askyesno("Confirmação", "Existem dados não salvos, voltar mesmo assim?")
+                if not confirmar:
+                    return
+            janela_votacao.destroy()
+            janela.deiconify() # Pop-up de verificação para voltar caso haja input preenchido
+
+        botao_voltar = tk.Button(janela_votacao, text="Voltar", bg="#e11c1c", fg="#ffffff", font=fonte_pequena,
+                                    command=voltar) # Linha 45; Destrói janela atual e desesconde a principal
+        botao_voltar.pack(padx=30, anchor="e") # Linha 46
+
+        label_menu = tk.Label(janela_votacao, text="Realize seu voto:", width=18, height=2,
+                                font=fonte_titulo) # "Caixa" p/ exibir texto ou imagem
+        label_menu.pack(pady=8) # Espaçamento entre o rótulo e os botões
+
         # Frame que envolve toda a janela
         frame_principal = tk.Frame(janela_votacao)
-        frame_principal.pack(fill="both", expand=True, padx=10, pady=10)
+        frame_principal.pack(fill="both", expand=True, pady=10)
 
         # Lado esquerdo
         frame_esquerda = tk.Frame(frame_principal)
-        frame_esquerda.grid(row=0, column=0, sticky="nsw", padx=10, pady=10)
+        frame_esquerda.pack(side="left", expand=True)
 
-        tk.Button(frame_esquerda, text="Voltar", command=lambda: (janela_votacao.destroy(), janela.deiconify())).pack(padx=10, pady=(20,5), anchor="w") # Linha
         tk.Label(frame_esquerda, text="Digite sua matrícula:", font=fonte_pequena).pack(padx=10, pady=5, anchor="w") # Linha 42
         entrada_matricula = tk.Entry(frame_esquerda)
         entrada_matricula.pack(padx=10, pady=5, anchor="w")
@@ -246,17 +303,13 @@ def registrar_voto():
             with open("candidatos.json", "w", encoding="utf-8") as arquivo:
                 json.dump(candidatos, arquivo, indent=4, ensure_ascii=False)
 
-    # Centro
-    frame_central = tk.Frame(frame_principal)
-    frame_central.grid(row=0, column=1, sticky="ne", padx=10, pady=10)
-
     # Linha
-    botao_votar = tk.Button(frame_central, text="Votar", command=confirmar_voto, bg="green") # Linha 45
+    botao_votar = tk.Button(janela_votacao, text="Votar", command=confirmar_voto, bg="green") # Linha 45
     botao_votar.pack(side="bottom", pady=10) # Linha
 
     # Lado direito
     frame_direita = tk.Frame(frame_principal)
-    frame_direita.grid(row=0, column=2, sticky="nsw", padx=10, pady=10)
+    frame_direita.pack(side="right", expand=True)
 
     tk.Label(frame_direita, text="Candidatos disponiveis", font=fonte_pequena).pack(pady=5) # Linha 42
 
@@ -276,7 +329,7 @@ def registrar_voto():
 
     canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
     canvas.pack(side="left", fill="both", expand=True)
-    scrollbar.pack(side="right", fill="y")
+    scrollbar.pack(side="left", fill="y", anchor="ne")
 
     # Exibir os candidatos com imagem + texto
     for c in candidatos:
@@ -291,10 +344,6 @@ def registrar_voto():
         label_info = tk.Label(frame_candidato, text=info, bg="white", justify="center") # Linha 42
         label_info.pack()
 
-    frame_principal.grid_columnconfigure(0, weight=0)
-    frame_principal.grid_columnconfigure(1, weight=1)
-    frame_principal.grid_columnconfigure(2, weight=2)
-
 def imprime_relatorio():
     if not votacao_ativa:
         messagebox.showinfo("Erro", "A votação não pode ser encerrada antes de ser iniciada.")
@@ -304,14 +353,17 @@ def imprime_relatorio():
     janela_relatorio.protocol("WM_DELETE_WINDOW", janela.destroy) # Linha 67
 
     # Definições básicas da janela atual
-    janela_relatorio.title("Resultados") # Linha 9
-    janela_relatorio.configure(bg='#f0f0f0') # linha 39
+    janela_relatorio.title("Urna eletrônica") # Linha 9
+    janela_relatorio.configure(bg='#f0f0f0', padx=30, pady=30) # linha 39
     janela_relatorio.geometry(f"{janela_largura}x{janela_altura}+{posx}+{posy}") # linha 40
+    janela_relatorio.resizable(False, False) # Linha 15
     
     # Conteúdo da janela atual
+    botao_voltar = tk.Button(janela_relatorio, text="Voltar", bg="#e11c1c", fg="#ffffff", font=fonte_pequena,
+            command=lambda: (janela_relatorio.destroy(), janela.deiconify)) # Linha 45; Destrói janela atual e desesconde a principal
+    botao_voltar.pack(anchor="e") # Linha 46
+
     total_votos = sum(c["votos"] for c in candidatos)
-    botao = tk.Button(janela_relatorio, text="Voltar", command=lambda: (janela.deiconify(), janela_relatorio.destroy())) # Linha 45; Linha 117
-    botao.pack(pady=5)
 
     # Preenche a página e o PDF com conteúdo
     if total_votos > 0:
@@ -359,7 +411,7 @@ def imprime_relatorio():
 
     tk.Button(janela_relatorio, text="Gerar PDF", command=gerar_pdf).pack() # Linha 45
 
-def encerrar_votacao():
+def ver_resultados():
     global votacao_ativa
     imprime_relatorio()
 
